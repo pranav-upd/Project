cc451c02e48dc8d7265cfb30ec6f89b79a741034
pranav-upd <pranav-upd@users.noreply.github.com>
2020-01-20 15:52:52 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -87,12 +87,26 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    int r=0,g=0,b=0,a=0,i2=0,j2=0,j3=0,r1=0,c1=0;
+    RGBTRIPLE image2[height][width];
+    for (int m=0;m<height;m++){
+        for (int n=0;n<width;n++){
+        image2[m][n].rgbtRed = image[m][n].rgbtRed;
+        image2[m][n].rgbtGreen = image[m][n].rgbtGreen;
+        image2[m][n].rgbtBlue = image[m][n].rgbtBlue;
+        }
+    }
+    int r=0,g=0,b=0,a=0,i2=0,j2=0,j3=0,r1=0,c1=0,r2=0,c2=0;
+    float avg=0,avg2=0;
     for(int i=0;i<height;i++){
         for(int j=0;j<width;j++){
-            int r2=3,c2=3;
-            float avg=0,avg2=0;
-            //...
+             r=0;
+             g=0;
+             b=0;
+             avg=0;
+             avg2=0;
+             c2 = 3;
+             r2 = 3;
+            //
             i2 = i;
             j2 = j;
             if (i2==0){
@@ -116,12 +130,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             j3 = j2;
             for(c1=0;c1<c2;c1++){
             for (r1=0;r1<r2;r1++){
-            r =  image[i2][j2].rgbtRed;
-            g = image[i2][j2].rgbtGreen;
-            b = image[i2][j2].rgbtBlue;
+            r = image2[i2][j2].rgbtRed;
+            g = image2[i2][j2].rgbtGreen;
+            b = image2[i2][j2].rgbtBlue;
             avg = r+g+b;
             avg = avg/3;
-            avg = round(avg);
             avg2 = avg2 + avg;
             j2 = j2 + 1;
             }
@@ -129,11 +142,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             j2 = j3;
             }
             avg2 = avg2/(r2*c2);
-            avg2 = round(avg2);
             a = avg2;
             image[i][j].rgbtRed=a;
             image[i][j].rgbtGreen=a;
             image[i][j].rgbtBlue=a;
+            printf("%d%d\t",c1,r1);
         }
     }
     return;

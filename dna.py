ce5e8121bfef18201b02459bcd6319140fa6d735
pranav-upd <pranav-upd@users.noreply.github.com>
2020-02-15 01:14:27 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -4,16 +4,44 @@ l = len(sys.argv)
 if l != 3:
     print("Usage: python file.csv file.txt")
     exit(1)
+#..
+b = 0
+#..
 csv_file = csv.DictReader(open(sys.argv[1]))
+txt_file = open(sys.argv[2])
+#..
+c = ""
+for b in txt_file:
+    c = c + b
+#..
+t = 0
 for data in csv_file:
     keydata = data.keys()
-#..
+    ln = len(keydata)
+    ln = ln - 1
+    for a in keydata:
+        if a != "name":
+           d = int(data[a])
+           n = c.count(a)
+           print(d,n)
+           if d == n:
+               t = t + 1
+           if t == ln:
+               print(data["name"])
+               exit(0)
+        if a == "name":
+            t = 0
+print("No match")
 
 
 
 
 
-txt_file = sys.argv[2]
+
+
+
+
+
 
 
 
